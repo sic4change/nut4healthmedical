c5c50d345acfe21e51bfed38a8a846995d990fc1
Aaron Asencio <aaron.asencio.tavio@gmail.com>
2022-03-16 15:57:09 +0000

    improve visual details

--- app/src/main/java/org/sic4change/nut4healthcentrotratamiento/ui/screens/cases/CaseListItem.kt
+++ app/src/main/java/org/sic4change/nut4healthcentrotratamiento/ui/screens/cases/CaseListItem.kt
@@ -3,6 +3,8 @@ package org.sic4change.nut4healthcentrotratamiento.ui.screens.cases
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Folder
+import androidx.compose.material.icons.filled.FolderOpen
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,7 +30,14 @@ fun  CaseListItem(
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                if (item.status == "open") {
+                if (item.status == stringResource(R.string.open)) {
+                    IconButton(onClick = {}) {
+                        Icon(
+                            tint = colorResource(R.color.colorAccent),
+                            imageVector = Icons.Default.FolderOpen,
+                            contentDescription = null
+                        )
+                    }
                     Text(
                         color = colorResource(R.color.colorAccent),
                         text = "${item.name}".toString().capitalize()  ,
@@ -46,6 +55,13 @@ fun  CaseListItem(
                         )
                     }
                 } else {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            tint = colorResource(R.color.colorPrimary),
+                            imageVector = Icons.Default.Folder,
+                            contentDescription = null
+                        )
+                    }
                     Text(
                         color = colorResource(R.color.colorPrimary),
                         text = "${item.name}".toString().capitalize()  ,
--- app/src/main/java/org/sic4change/nut4healthcentrotratamiento/ui/screens/cases/detail/CaseDetailScreen.kt
+++ app/src/main/java/org/sic4change/nut4healthcentrotratamiento/ui/screens/cases/detail/CaseDetailScreen.kt
@@ -131,23 +131,44 @@ private fun Header(caseState: CaseState) {
                 Icon(Icons.Default.CalendarToday, null, tint = colorResource(R.color.colorPrimary),  modifier = Modifier.clickable { })},
                     label = { Text(stringResource(R.string.last_date), color = colorResource(R.color.disabled_color)) })
         Spacer(modifier = Modifier.height(16.dp))
-        TextField(value = caseState.status.value.toString().capitalize(),
-            onValueChange = {}, readOnly = true,
-            colors = TextFieldDefaults.textFieldColors(
-                textColor = colorResource(R.color.colorPrimary),
-                backgroundColor = colorResource(androidx.browser.R.color.browser_actions_bg_grey),
-                cursorColor = colorResource(R.color.full_transparent),
-                disabledLabelColor =  colorResource(androidx.browser.R.color.browser_actions_bg_grey),
-                focusedIndicatorColor = colorResource(R.color.full_transparent),
-                unfocusedIndicatorColor = colorResource(R.color.full_transparent),
-            ),
-            textStyle = MaterialTheme.typography.h5,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp, 0.dp),
-            leadingIcon = {
-                Icon(Icons.Filled.Folder, null, tint = colorResource(R.color.colorPrimary),  modifier = Modifier.clickable { })},
-                    label = { Text(stringResource(R.string.status), color = colorResource(R.color.disabled_color)) })
+        if (caseState.status.value.toString() == stringResource(R.string.open)) {
+            TextField(value = caseState.status.value.toString().capitalize(),
+                onValueChange = {}, readOnly = true,
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = colorResource(R.color.colorAccent),
+                    backgroundColor = colorResource(androidx.browser.R.color.browser_actions_bg_grey),
+                    cursorColor = colorResource(R.color.full_transparent),
+                    disabledLabelColor =  colorResource(androidx.browser.R.color.browser_actions_bg_grey),
+                    focusedIndicatorColor = colorResource(R.color.full_transparent),
+                    unfocusedIndicatorColor = colorResource(R.color.full_transparent),
+                ),
+                textStyle = MaterialTheme.typography.h5,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp, 0.dp),
+                leadingIcon = {
+                    Icon(Icons.Filled.FolderOpen, null, tint = colorResource(R.color.colorAccent),  modifier = Modifier.clickable { })},
+                label = { Text(stringResource(R.string.status), color = colorResource(R.color.disabled_color)) })
+        } else {
+            TextField(value = caseState.status.value.toString().capitalize(),
+                onValueChange = {}, readOnly = true,
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = colorResource(R.color.colorPrimary),
+                    backgroundColor = colorResource(androidx.browser.R.color.browser_actions_bg_grey),
+                    cursorColor = colorResource(R.color.full_transparent),
+                    disabledLabelColor =  colorResource(androidx.browser.R.color.browser_actions_bg_grey),
+                    focusedIndicatorColor = colorResource(R.color.full_transparent),
+                    unfocusedIndicatorColor = colorResource(R.color.full_transparent),
+                ),
+                textStyle = MaterialTheme.typography.h5,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp, 0.dp),
+                leadingIcon = {
+                    Icon(Icons.Filled.Folder, null, tint = colorResource(R.color.colorPrimary),  modifier = Modifier.clickable { })},
+                label = { Text(stringResource(R.string.status), color = colorResource(R.color.disabled_color)) })
+        }
+
         Spacer(modifier = Modifier.height(16.dp))
         TextField(value = caseState.visits.value,
             onValueChange = {}, readOnly = true,
