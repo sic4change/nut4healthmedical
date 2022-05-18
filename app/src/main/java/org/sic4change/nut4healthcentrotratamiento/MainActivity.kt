c29606f81eac252d6acab12cf10d7f1fab47dc7a
Aaron Asencio <aaron.asencio.tavio@gmail.com>
2022-05-18 15:34:12 +0100

    prepare to receive push notification

--- app/src/main/java/org/sic4change/nut4healthcentrotratamiento/MainActivity.kt
+++ app/src/main/java/org/sic4change/nut4healthcentrotratamiento/MainActivity.kt
@@ -13,6 +13,7 @@ import org.sic4change.nut4healthcentrotratamiento.ui.theme.NUT4HealthTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 import org.sic4change.nut4healthcentrotratamiento.ui.NUT4HealthApp
 
@@ -23,9 +24,18 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        notificationSetting()
         setContent {
             NUT4HealthApp()
+        }
+    }
+
+    private fun notificationSetting() {
+        FirebaseMessaging.getInstance().subscribeToTopic("rosa_del_desierto")
+        val childId: String? = intent.getStringExtra("childId")
+        childId?.let {
+            println("Ha llegado una notificación push del niño: ${childId}")
         }
     }
 }
---
+++ app/src/main/java/org/sic4change/nut4healthcentrotratamiento/NUT4HealthCentroTratamientoMessagingService.kt
@@ -0,0 +1,8 @@
+package org.sic4change.nut4healthcentrotratamiento
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+
+class NUT4HealthCentroTratamientoMessagingService: FirebaseMessagingService() {
+
+}
