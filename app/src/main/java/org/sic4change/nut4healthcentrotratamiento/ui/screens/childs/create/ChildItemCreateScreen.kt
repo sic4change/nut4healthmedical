b2e2e62f464baf859bf0673780c2a56c5e7a05e7
Aaron Asencio <aaron.asencio.tavio@gmail.com>
2023-06-13 09:50:22 +0100

    clean code

--- app/src/main/java/org/sic4change/nut4healthcentrotratamiento/ui/screens/childs/create/ChildItemCreateScreen.kt
+++ app/src/main/java/org/sic4change/nut4healthcentrotratamiento/ui/screens/childs/create/ChildItemCreateScreen.kt
@@ -74,7 +74,7 @@ private fun Header(childState: ChildState,
             R.string.beydan), stringResource(R.string.haratin), stringResource(R.string.soninke),
         stringResource(R.string.autre))
 
-    val brotherOptions = listOf(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20, 21)
+    val brotherOptions = listOf(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21)
 
     Column(
         modifier = Modifier.fillMaxWidth()
