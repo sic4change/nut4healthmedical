21fac15cb6ae5e28e79d1167f024cd754a7f6678
Aaron Asencio <aaron.asencio.tavio@gmail.com>
2023-06-14 10:34:35 +0100

    add icon in tab header

--- app/src/main/java/org/sic4change/nut4healthcentrotratamiento/ui/NUT4HealthApp.kt
+++ app/src/main/java/org/sic4change/nut4healthcentrotratamiento/ui/NUT4HealthApp.kt
@@ -3,35 +3,31 @@ package org.sic4change.nut4healthcentrotratamiento.ui
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
 import coil.annotation.ExperimentalCoilApi
 import com.aaronat1.hackaton.ui.navigation.NavCommand
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 import org.sic4change.nut4healthcentrotratamiento.MainActivity
 import org.sic4change.nut4healthcentrotratamiento.R
 import org.sic4change.nut4healthcentrotratamiento.ui.navigation.AppBarIcon
 import org.sic4change.nut4healthcentrotratamiento.ui.navigation.DrawerContent
 import org.sic4change.nut4healthcentrotratamiento.ui.navigation.Feature
 import org.sic4change.nut4healthcentrotratamiento.ui.navigation.Navigation
-import org.sic4change.nut4healthcentrotratamiento.ui.screens.main.rememberMainState
 import org.sic4change.nut4healthcentrotratamiento.ui.theme.NUT4HealthTheme
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -58,7 +54,13 @@ fun NUT4HealthApp() {
                             }
                         },
                         backgroundColor = colorResource(R.color.colorPrimary),
-                        title = { },
+                        title = {
+                            Image(
+                                modifier = Modifier.fillMaxWidth().size(120.dp, 120.dp),
+                                painter = painterResource(id = R.mipmap.ic_logo_header),
+                                contentDescription = null,
+                            )
+                        },
                         navigationIcon = {
                             if (!appState.currentRoute.contains("login/detail") &&
                                 !appState.currentRoute.contains("settings/home") &&
@@ -75,8 +77,6 @@ fun NUT4HealthApp() {
                             }
 
                         },
-
-
                         )
                 },
 
